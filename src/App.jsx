e83b19ab2fe13d8cedf7519c086c7930f5d768fe
JulioCssar <jc661513@gmail.com>
2023-09-01 15:53:43 -0300

    att com barra de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [subTitulo, setSubTitulo] = useState("")
+  const [searchTerm, setSearchTerm] = useState('');
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const [produtos, setProdutos] = useState([])
+  useEffect(() => {
+    axios.get('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+      .then((response) => {
+        setProducts(response.data);
+        setFilteredProducts(response.data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar produtos:', error);
+      });
+  }, []);
 
-  async function handleGetProducts() {
-    const { data } = await axios.get("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline") //Api usada 
-    setProdutos(data)
-  }
+  useEffect(() => {
+    if (searchTerm === '') {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  }, [searchTerm, products]);
 
-  useEffect(() => { //Save products
-    handleGetProducts()
-  }, [])
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-  return (  //Retorno
-    <>
-      <div className='topBar'>
-        <img id='logo' src="src/assets/logo/logo.png" alt="Logo"/>
+  return (
+    <div className="App">
+      <div className="topBar">
+        {/* <img id="logo" src="src/assets/img/logo.png" alt="Logo" /> */}
+        <h1>Confira</h1>
+        <input id='input'
+          type="text"
+          placeholder="Pesquisar produtos"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
       </div>
       <div className="container">
-        {
-          produtos.map((item) => (
-            <div className="card">
-              <img src={item.image_link} alt={item.name} width={100} />
-              <span className="titulo">{item.name}</span>
-              <span className="texto">R$ {item.price}</span>
-            </div>
-          ))
-        }
+        {filteredProducts.map((product) => (
+          <div key={product.id} className="card">
+            <img src={product.image_link} alt={product.name} />
+            <h3 className="titulo">{product.name}</h3>
+            <p className="texto">R$ {product.price}</p>
+          </div>
+        ))}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+
