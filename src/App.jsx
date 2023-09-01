92c3c2ee8bbc6d21059c8ac6adcf596b140a71ce
JulioCssar <jc661513@gmail.com>
2023-09-01 13:42:57 -0300

    deploy in github

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import "./App.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [subTitulo, setSubTitulo] = useState("")
+
+  const [produtos, setProdutos] = useState([])
+
+  async function handleGetProducts() {
+    const { data } = await axios.get("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline") //Api usada 
+    setProdutos(data)
+  }
+
+  useEffect(() => { //Save products
+    handleGetProducts()
+  }, [])
+
+  return (  //Retorno
+    <>
+      <div className='topBar'>
+        <img id='logo' src="src/assets/logo/logo.png" alt="Logo da marca" />
+      </div>
+      <div className="container">
+        {
+          produtos.map((item) => (
+            <div className="card">
+              <img src={item.image_link} alt={item.name} width={100} />
+              <span className="titulo">{item.name}</span>
+              <span className="texto">R$ {item.price}</span>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
